a96ba7916293e20564c24c073e4cfe2300c20d8a
anarkia1976 <stefano.villa1976@gmail.com>
2013-10-25 14:52:41 +0200

    msm: kgsl: rework naming convention for gpu undervolt userspace
    Conflicts:
    	drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -659,12 +659,12 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
-ssize_t show_GPU_mV_table(struct cpufreq_policy *policy, char *buf)
+ssize_t show_gpu_mv_table(struct cpufreq_policy *policy, char *buf)
 {
         return get_gpu_vdd_levels_str(buf);
 }
 
-ssize_t store_GPU_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count)
+ssize_t store_gpu_mv_table(struct cpufreq_policy *policy, const char *buf, size_t count)
 {
         unsigned int ret = -EINVAL;
         unsigned int u[3];
@@ -689,7 +689,7 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-cpufreq_freq_attr_rw(GPU_mV_table);
+cpufreq_freq_attr_rw(gpu_mv_table);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -705,7 +705,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-	&GPU_mV_table.attr,
+	&gpu_mv_table.attr,
 	NULL
 };
 
