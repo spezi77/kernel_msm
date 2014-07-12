8aede8a6e00a9c50d453258cce32a7056742beb4
yank555-lu <yank555.lu@gmail.com>
2014-07-12 16:37:55 +0200

    cpufreq: add yankdemand governor
    Full stock (JB) ondemand governor with changed default tunable values aimed at lower battery consumption
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,6 +391,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_STOCKDEMAND)
 extern struct cpufreq_governor cpufreq_gov_stockdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_stockdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
+extern struct cpufreq_governor cpufreq_gov_yankdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
 #endif
 
 
