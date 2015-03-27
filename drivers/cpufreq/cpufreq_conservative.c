32c0e621a5c87267fa5b5621366b68881e31e24c
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-27 03:38:25 +0000

    cpufreq: conservative: don't tear up sysfs and governor dirs to prevent selinux errors and preserve file permissions properly
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -502,7 +502,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 			rc = sysfs_create_group(cpufreq_global_kobject,
 						&dbs_attr_group);
-			if (rc) {
+			if (rc && rc != -EEXIST) {
 				mutex_unlock(&dbs_mutex);
 				return rc;
 			}
@@ -547,9 +547,6 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 					CPUFREQ_TRANSITION_NOTIFIER);
 
 		mutex_unlock(&dbs_mutex);
-		if (!dbs_enable)
-			sysfs_remove_group(cpufreq_global_kobject,
-					   &dbs_attr_group);
 
 		break;
 
