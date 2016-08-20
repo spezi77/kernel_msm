7530dc675aa2ef709da33e255b41dbc77d977724
spezi77 <spezi77@gmx.com>
2016-08-20 23:06:15 +0200

    cpufreq: yankdemand: switch to predefined (standard) work queue.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- drivers/cpufreq/cpufreq_yankdemand.c
+++ drivers/cpufreq/cpufreq_yankdemand.c
@@ -464,7 +464,7 @@ static void update_sampling_rate(unsigned int new_rate)
 			cancel_delayed_work_sync(&dbs_info->work);
 			mutex_lock(&dbs_info->timer_mutex);
 
-			queue_delayed_work_on(dbs_info->cpu, dbs_wq,
+			schedule_delayed_work_on(dbs_info->cpu,
 				&dbs_info->work, usecs_to_jiffies(new_rate));
 
 		}
@@ -1576,7 +1576,7 @@ static void do_dbs_timer(struct work_struct *work)
 			dbs_info->freq_lo, CPUFREQ_RELATION_H);
 		delay = dbs_info->freq_lo_jiffies;
 	}
-	queue_delayed_work_on(cpu, dbs_wq, &dbs_info->work, delay);
+	schedule_delayed_work_on(cpu, &dbs_info->work, delay);
 	mutex_unlock(&dbs_info->timer_mutex);
 }
 
@@ -1590,7 +1590,7 @@ static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 
 	dbs_info->sample_type = DBS_NORMAL_SAMPLE;
 	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
-	queue_delayed_work_on(dbs_info->cpu, dbs_wq, &dbs_info->work, delay);
+	schedule_delayed_work_on(dbs_info->cpu, &dbs_info->work, delay);
 }
 
 static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)
@@ -1760,7 +1760,7 @@ static int dbs_sync_thread(void *data)
 
 			/* reschedule the next yankdemand sample */
 			mutex_lock(&this_dbs_info->timer_mutex);
-			queue_delayed_work_on(cpu, dbs_wq,
+			schedule_delayed_work_on(cpu,
 					      &this_dbs_info->work, delay);
 			mutex_unlock(&this_dbs_info->timer_mutex);
 		}
